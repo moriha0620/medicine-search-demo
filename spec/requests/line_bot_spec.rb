fed72dd3cec15eb1edd6f2832b01a76dc7a1ae7a
moriha0620 <skk0620367@gmail.com>
2021-04-23 12:29:12 +0900

    READMEの修正１

--- spec/requests/line_bot_spec.rb
+++ spec/requests/line_bot_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe "line_bot/callback", type: :request do
   describe "POST /line_bot/callback" do
     it "callbackアクションへリクエストされると正常にレスポンスを返すことができる" do
       post callback_path
-      binding.pry
+      expect(response).to have_http_status(400)
     end
   end
 end
